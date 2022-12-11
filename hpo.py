7dfbd904c125bbafc394f6e51a4d996941c1c044
Saumil Apurva Shah <sashah8@ncsu.edu>
2022-12-10 20:26:03 -0500

    Tuner changed

--- hpo.py
+++ hpo.py
@@ -1,5 +1,4 @@
 # Importing required libraries
-import sys
 import json
 from nni.experiment import Experiment
 
@@ -19,33 +18,17 @@ experiment.config.trial_code_directory = '.'
 # Configuring the search space
 experiment.config.search_space = search_space
 
-# This tuner needs additional install -> pip install nni[BOHB]
-# experiment.config.tuner.name = 'BOHB'
-experiment.config.tuner.name = 'TPE'
-
-# experiment.config.tuner.class_args = {
-#     'optimize_mode': 'maximize',
-#     'min_budget': 1,
-#     'max_budget': 600,
-#     'eta': 3,
-#     'min_points_in_model': 7,
-#     'top_n_percent': 15,
-#     'num_samples': 64,
-#     'random_fraction': 0.33,
-#     'bandwidth_factor': 3.0,
-#     'min_bandwidth': 0.001
-# }
-# experiment.config.tuner.name = 'Hyperband'
-# experiment.config.tuner.class_args = {
-#     'optimize_mode': 'maximize',
-#     'R': 60,
-#     'eta': 3
-# }
+# Defining the HPO tuner algorithm to be used
+experiment.config.tuner.name = 'Evolution'
+experiment.config.tuner.class_args = {
+        'optimize_mode': 'maximize',
+        'population_size': 125
+}
 
 experiment.config.debug = True
 
 # Setting a name for the experiment
-experiment.config.experiment_name = f'TPE - Detailed config and tuner version'
+experiment.config.experiment_name = f'VDSR HPO'
 
 # Setting up number of trials to run -> Sets of hyperparameters and trial concurrency
 experiment.config.max_trial_number = 50  # Change to a higher number -> 50
